61565ed3a8e232d73a3e886832de8ba6d064afca
Slowist Lee <3230103034@zju.edu.cn>
2024-06-28 11:59:41 +0800

    Deployed d39625b3 with MkDocs version: 1.6.0

--- password.js
+++ password.js
@@ -1,14 +1,18 @@
-// password-protect.js
 function checkPassword() {
     var password = prompt("请输入密码:");
     if (password !== "lx051607") {
         alert("密码错误，访问被拒绝。");
-        window.location.href = "access-denied.html";
+        window.location.href = "/access-denied.html";
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    if (window.location.pathname.endsWith("杂项&思考/随便写写/各种message的草稿/")) {
+    var protectedPaths = [
+        "/杂项&思考/随便写写/各种message的草稿/"
+    ];
+    
+    var currentPath = window.location.pathname;
+    if (protectedPaths.some(path => currentPath.endsWith(path))) {
         checkPassword();
     }
 });
