b4fc23bd3066e0f5c75488db3a9a7bfe6acbeb5a
Slowist Lee <3230103034@zju.edu.cn>
2024-06-28 13:34:50 +0800

    Deployed 525bc6ee with MkDocs version: 1.6.0

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var sections = document.querySelectorAll('.md-nav__item--nested');
-
-  sections.forEach(function(section) {
-      var header = section.querySelector('.md-nav__link');
-      var subItems = section.querySelector('.md-nav__drop');
-
-      if (header && subItems) {
-          // Initially hide the sub-items
-          subItems.style.display = 'none';
-
-          // Toggle the display on header click
-          header.addEventListener('click', function(event) {
-              event.preventDefault();
-              if (subItems.style.display === 'none') {
-                  subItems.style.display = 'block';
-              } else {
-                  subItems.style.display = 'none';
-              }
-          });
-      }
-  });
-});
-
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml3');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
